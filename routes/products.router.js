f7fe717b848c4ac848193d56af470c0daba16fe1
Luciano Massa <lgmassa98@gmail.com>
2022-06-27 11:08:19 -0300

    separacio de responsabilidades

---
+++ routes/products.router.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',(req,res)=>{
+  const products=[{},{}];
+  const{size}=req.query;
+   res.json(products);
+});
+
+router.get('/filter',(req,res)=>{
+  res.send('Yo soy un filter');
+});
+
+router.get('/:id',(req,res)=>{
+  const{id}=req.params;
+  res.json({id,name:'Product 2',price:2000})
+;});
+
+module.exports=router;
