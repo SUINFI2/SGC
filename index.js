b8eb18459e35fbf02aca66bdc7c3ff0abf362d66
Luciano Massa <lgmassa98@gmail.com>
2022-07-08 10:35:55 -0300

    persistencia de datos

--- index.js
+++ index.js
@@ -36,28 +36,34 @@ app.listen(port, ()=>{
 -> Compras= {
 
   ** v0.1 **
-  -> realizar una {nota de pedido} enviada a un {prveedor} y un {encargado de compras} para
-     luego confirmarla el {encargado de deposito} de esta manera modificando el {deposito} y
-     genenrando una {orden de pago} recibida por un {encargado de tesoreria} y asi modificar {cuentas}.
-          // la nota de pedido puede ser de materia prima y/o mercaderia.
-          // el encargado de compras puede ser de produccion y/o mercaderia.
-          // el deposito puede almacenar materia prima, produccion y/o mercaderia.
+  -> crear compra directa que afecte stock
+  -> crear compra indirecta
+  }
 
-  **
-}
-
-
- ventas = {
-
+  ventas = {
   ** v0.1 **
-  -> realizar una {venta_directa} por un {cajero}, para un{cliente}
-     modificando el {deposito},genenrando una {orden de cobro} recibida por un
-     {encargado de tesoreria} y asi modificar {cuentas}.
+  -> crear venta directa, que afecte stock
 
-  ** v0.2 **
-  -> -> realizar un {pedido_venta} para ser manipulado por {encargado deposito} y despues ser entregado
-     a {encargado reparto}, este luego debera entregarlo a {cliente} para finalizar la entrega y modificar {deposito}
+ cobros = {
+  ** v0.1 **
+  -> crear orden de cobro que afecte a las cuentas
 
+ }
+
+  pagos = {
+  ** v0.1 **
+  -> crear orden de pago que afecte a las cuentas
+
+ }
+
+ cuentas={
+    ** v0.1 **
+  -> poder visualizar eñ estado de cuentas
+  ->
+ }
+ impresora = {
+  ** v0.1 **
+  -> elegir impresora e imprimir ventas, compras o informes
 
  }
 */
