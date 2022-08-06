223b352a4b34273cf3675fe6e2b6df9f0adbf2b3
Luciano Massa <lgmassa98@gmail.com>
2022-08-06 19:45:03 -0300

    prueba 3

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,27 +1,23 @@
-console.log('aqui 11..........');
 const { Sequelize } = require('sequelize');
-console.log('aqui 1a1..........');
-const { config } = require('../config/config');
-const setupModels = require('../db/models');
+
+const { config } = require('./../config/config');
+const setupModels = require('./../db/models');
 
 const options = {
   dialect: 'postgres',
   logging: config.isProd ? false : true,
 }
-console.log('aqui 21..........');
+
 if (config.isProd) {
   options.dialectOptions = {
     ssl: {
-      require: true,
       rejectUnauthorized: false
     }
   }
 }
-console.log('aqui 22'+config.isProd);
-const dat = new Sequelize(config.dbUrl,options);
 
-console.log('aqui 23');
-setupModels(dat);
+const sequelize = new Sequelize(config.dbUrl, options);
 
+setupModels(sequelize);
 
-module.exports = dat;
+module.exports = sequelize;
